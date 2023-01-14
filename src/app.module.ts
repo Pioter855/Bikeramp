709bfb6291a968d2ad31942e3f7174b28a2f4002
Pioter <piotrklis.85@o2.pl>
2023-01-14 16:23:13 +0000

    Connecting the configuration module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { typeOrmConfig } from "./config/database.config";
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    envFilePath: '.env'
+    }),
+  TypeOrmModule.forRoot(typeOrmConfig)],
   controllers: [],
   providers: [],
 })
---
+++ src/config/database.config.ts
@@ -0,0 +1,14 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { config } from 'dotenv';
+
+config()
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: +process.env.DATABASE_PORT,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_DATABASE,
+  entities: [__dirname + '/../**/*.entity.ts'],
+  synchronize: true,
+};
